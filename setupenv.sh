6d7e1b30fe6c2e4e46045b41bcb41b1eec6cc7ec
David Wang <david.wang5766@gmail.com>
2024-10-08 21:15:31 -0700

    add setupenv script

--- setupenv.sh
+++ setupenv.sh
@@ -3,7 +3,10 @@ export SHOPPING="http://george.cs.ucsb.edu:7770";
 export SHOPPING_ADMIN="http://george.cs.ucsb.edu:7780/admin";
 export REDDIT="http://george.cs.ucsb.edu:9999";
 export GITLAB="http://george.cs.ucsb.edu:8023";
-export MAP="http://george.cs.ucsb.edu:3000";
+export MAP="http://ec2-3-131-244-37.us-east-2.compute.amazonaws.com:3000";
 export WIKIPEDIA="http://george.cs.ucsb.edu:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing";
 # The home page is not currently hosted in the demo site
 export HOMEPAGE="PASS"  # set this if HOMEPAGE is hosted later
+
+
+python browser_env/auto_login.py
